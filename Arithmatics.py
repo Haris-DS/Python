c7b09d4d18ddd00329b8e98ed5f8ae5d970f927e
Haris Habib <harishabib2@gmail.com>
2025-08-04 16:15:33 +0500

    Arithmatic operators and Maths Module in Python

---
+++ Arithmatics.py
@@ -0,0 +1,66 @@
+# Math Module in Python
+# import math
+
+
+# print(math.pi)  # Print the value of pi
+# print(math.e)   # Print the value of e
+# print(math.sqrt(16))  # Calculate the square root of 16
+# print(math.factorial(5))  # Calculate the factorial of 5
+# print(math.pow(2, 3))  # Calculate 2 raised to the power of 3
+# print(math.ceil(4.2))  # Round up to the nearest integer
+# print(math.floor(4.8))  # Round down to the nearest integer
+# print(math.gcd(12, 15))  # Calculate the greatest common divisor of 12 and 15
+# print(math.sin(math.pi / 2))  # Calculate the sine of pi/2
+# print(math.cos(0))  # Calculate the cosine of 0
+# print(math.tan(math.pi / 4))  # Calculate the tangent of pi/4
+
+
+# # Radius of a circle
+# radius = float(input("Enter the radius of the circle: "))
+# circumference = 2 * math.pi * radius
+# print(f"The circumference of the circle is: {circumference:.2f}")
+# area = math.pi * radius ** 2
+# print(f"The area of the circle is: {area:.2f}")
+
+# # Fiind the hypotenuse of a right triangle
+# a = float(input("Enter the length of side a: "))
+# b = float(input("Enter the length of side b: "))
+# hypotenuse = math.sqrt(a ** 2 + b ** 2)
+# print(f"The length of the hypotenuse is: {hypotenuse:.2f}")
+
+# Exploring Artihmatics Operations in Python
+
+# friends = 10
+# print("Initial Friends count:", friends)
+
+# friedns = friends + 1
+
+# argumented assignment operator
+# friends += 1
+# friends -= 1
+# friends *= 5
+# friends /= 2
+# friends //= 2
+# friends %= 3
+# friends **= 2
+# print("Friends count:", friends)
+
+
+# x = 3.14
+# y = 4
+# z = 10
+
+# result = round(x)
+# print("Rounded value of x:", result)
+# result = abs(-x)
+# print("Absolute value of -x:", result)
+# result = pow(x, y)
+# print("x raised to the power of y:", result)
+# result = max(x, y, z)
+# print("Maximum value among x, y, z:", result)
+# result = min(x, y, z)
+# print("Minimum value among x, y, z:", result)
+# result = divmod(x, y)
+# print("Quotient and remainder of x divided by y:", result)
+# result = sum([x, y, z])
+# print("Sum of x, y, z:", result)
