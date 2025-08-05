414d21943f656ce665d8a425c6b1c712da1587b5
Haris Habib <harishabib2@gmail.com>
2025-08-05 15:06:56 +0500

    Format Specifiers iin Python

---
+++ format_specifiers.py
@@ -0,0 +1,34 @@
+# Format Spacifiers in Python
+price1 = 4990.9934
+price2 = 1923.9922
+price3 = 5000.4924
+
+# Decimals
+
+# print(f"Price1 is: ${price1:.2f}")
+# print(f"Price2 is: ${price2:.2f}")
+# print(f"Price3 is: ${price3:.2f}")
+
+# Spaces
+
+# print(f"Price1 is: ${price1:10}")
+# print(f"Price2 is: ${price2:10}")
+# print(f"Price3 is: ${price3:10}")
+
+# ALignment
+
+# print(f"Price1 is: ${price1:<10}")
+# print(f"Price2 is: ${price2:<10}")
+# print(f"Price3 is: ${price3:<10}")
+# print(f"Price1 is: ${price1:>10}")
+# print(f"Price2 is: ${price2:>10}")
+# print(f"Price3 is: ${price3:>10}")
+# print(f"Price1 is: ${price1:^10}")
+# print(f"Price2 is: ${price2:^10}")
+# print(f"Price3 is: ${price3:^10}")
+
+# 1000s
+
+# print(f"Price1 is: ${price1:,.2f}")
+# print(f"Price2 is: ${price2:,.2f}")
+# print(f"Price3 is: ${price3:,.2f}")
