53bae6bc6a168d79a962544849a1270283d52db8
Haris Habib <harishabib2@gmail.com>
2025-08-05 15:39:54 +0500

    while loop and it's exercise

---
+++ Exercise_Compond_Calc.py
@@ -0,0 +1,50 @@
+# Compound Calculator
+
+principal = 0
+rate = 0
+time = 0
+
+# while principal <= 0:
+#     principal = float(input("Enter the principal amount (greater than 0): "))
+#     if principal <= 0:
+#         print("Principal must be greater than 0. Please try again.")
+
+
+# while rate <= 0:
+#     rate = float(input("Enter the Interest Rate amount (greater than 0): "))
+#     if rate <= 0:
+#         print("Interest Rate must be greater than 0. Please try again.")
+
+# while time <= 0:
+#     time = int(input("Enter the Time (in Years): "))
+#     if rate <= 0:
+#         print("Time must be greater than 0. Please try again.")
+
+
+# If user Want to enter 0
+
+while True:
+    principal = float(input("Enter the principal amount (greater than 0): "))
+    if principal < 0:
+        print("Principal must be greater than 0. Please try again.")
+    else:
+        break
+
+while True:
+    rate = float(input("Enter the Interest Rate amount (greater than 0): "))
+    if rate < 0:
+        print("Interest Rate must be greater than 0. Please try again.")
+    else:
+        break
+
+while True:
+    time = int(input("Enter the Time (in Years): "))
+    if rate < 0:
+        print("Time must be greater than 0. Please try again.")
+    else:
+        break
+
+
+total = principal * (1 + rate / 100) ** time
+print(f"Total amount after {time} years is: ${total:.2f}")
+print(f"Interest earned is: ${total - principal:.2f}")
---
+++ While_loop.py
@@ -0,0 +1,35 @@
+# While Loop
+
+# name = input("Enter your name: ")
+# if name == "":
+#     print("You didn't enter your name.")
+# else:
+#     print(f"Hello {name}!")
+
+
+# while name == "":
+#     print("You didn't enter your name.")
+#     name = input("Please enter your name: ")
+# print(f"Hello {name}!")
+
+
+# age = int(input("Enter your age: "))
+# while age < 0:
+#     print("Age cannot be negative. Please enter a valid age.")
+#     age = int(input("Enter your age: "))
+# print(f"Your age is {age}.")
+
+
+# food = input("Enter your favorite food (q to quit): ")
+# while not food == "q":
+#     print(f"Your favorite food is {food}.")
+#     food = input("Enter your another food you like (q to quit): ")
+# print("Thank you for sharing your favorite food!")
+
+
+# num = int(input("Enter a number from 1-10: "))
+# while num < 1 or num > 10:
+#     print(f"{num} is not between 1 and 10. Please try again.")
+#     num = int(input("Enter a number from 1-10: "))
+
+# print(f"Your number is: {num}.")
