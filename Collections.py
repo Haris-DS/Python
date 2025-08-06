932cde8f920c455f6eef37af375618d1156668e5
Haris Habib <harishabib2@gmail.com>
2025-08-06 16:38:50 +0500

    Lists

---
+++ Collections.py
@@ -0,0 +1,42 @@
+# Collections = Single "Variable" can store multiple values
+# List, Tuple, Set, Dictionary
+# List [] = Ordered, Mutable (Changable), Allows Duplicates
+# Tuple () = Ordered, Immutable(Not Changable), Allows Duplicates
+# Set {}= Unordered, Mutable, No Duplicates
+# Dictionary {Key:Value} = Unordered, Mutable, Key-Value Pairs
+
+
+# list
+
+# fruits = ["apple", "banana", "cherry"]
+# print(help(fruits))
+# print(dir(fruits))
+# # Accessing elements
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+
+# for x in fruits:
+#     print(x)
+# # Adding elements
+# fruits.append("orange")
+# print(fruits)
+# # Removing elements
+# fruits.remove("banana")
+# print(fruits)
+# # Length of list
+# print(len(fruits))
+# # Check if an item exists
+# if "apple" in fruits:
+#     print("Yes, 'apple' is in the fruits list")
+# # Sorting the list
+# fruits.sort()
+# print(fruits)
+# # Reversing the list
+# fruits.reverse()
+# print(fruits)
+# fruits.insert(1, "kiwi")
+# print(fruits)
+# Copying the list
+# fruits_copy = fruits.copy()
+# print(fruits_copy)
