a7fb14e4a4789a95e829ab063ce824125dd5cc5e
Haris Habib <harishabib2@gmail.com>
2025-08-07 20:21:50 +0500

    Random Module in Python

---
+++ random_numers.py
@@ -0,0 +1,15 @@
+# import random
+
+# print(help(random))
+# low = 1
+# high = 100
+# number = random.randint(low, high)
+# print(number)
+
+# options = ("rock", "papper", "scissors")
+# options = random.choice(options)
+# print(options)
+
+# cards = ["A", "K", "Q", "J", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+# shuffle = random.shuffle(cards)
+# print(cards)
