bbd5e7d447bc07eebe3d39536b5cc3404473ff45
Haris Habib <harishabib2@gmail.com>
2025-08-08 16:11:41 +0500

    Dice roller game in python

---
+++ dice_roller.py
@@ -0,0 +1,64 @@
+# Dice roller program in python
+
+import random
+
+dice_faces = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"
+    ),
+    2: (
+        "┌─────────┐",
+        "│ ●       │",
+        "│         │",
+        "│       ● │",
+        "└─────────┘"
+    ),
+    3: (
+        "┌─────────┐",
+        "│ ●       │",
+        "│    ●    │",
+        "│       ● │",
+        "└─────────┘"
+    ),
+    4: (
+        "┌─────────┐",
+        "│ ●     ● │",
+        "│         │",
+        "│ ●     ● │",
+        "└─────────┘"
+    ),
+    5: (
+        "┌─────────┐",
+        "│ ●     ● │",
+        "│    ●    │",
+        "│ ●     ● │",
+        "└─────────┘"
+    ),
+    6: (
+        "┌─────────┐",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "│ ●     ● │",
+        "└─────────┘"
+    )
+}
+
+dice = []
+total = 0
+num_of_dice = int(input("how Many Dice?: "))
+
+for i in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+for i in range(num_of_dice):
+    for j in dice_faces.get(dice[i]):
+        print(j)
+
+
+for i in dice:
+    total += i
+print(f"Total: {total}")
