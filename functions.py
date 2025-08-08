d35ec2e11accdbdd93de2323418cdd73e2c87810
Haris Habib <harishabib2@gmail.com>
2025-08-08 18:21:18 +0500

    finction in Python

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Functions in Python
+
+# def happy_birthday(name, age):
+#     print(f"Happy Birthday {name}")
+#     print(f"You are {age} now!")
+
+
+# happy_birthday('Haris', 30)
+
+# def greet(name):
+#     print(f"Hello, {name}!")
+
+
+# greet("haris")
