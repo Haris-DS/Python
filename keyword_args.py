5ec8d59a9d77a66b767f97c4894b0ee805701284
Haris Habib <harishabib2@gmail.com>
2025-08-12 14:55:19 +0500

    Keyword Arguments in Python

--- keyword_args.py
+++ keyword_args.py
@@ -4,8 +4,8 @@
 #                     1. Positional, 2. Default, 3. Keyword, 4. Arbitrary
 
 
-def hello(greeting, title, fname, lname):
-    print(f"{greeting} {title}.{fname} {lname}")
+# def hello(greeting, title, fname, lname):
+#     print(f"{greeting} {title}.{fname} {lname}")
 
 
 # arguments by position
@@ -13,3 +13,18 @@ def hello(greeting, title, fname, lname):
 
 # arguments by keyword
 # print(hello("Hello", "Mr", lname="James", fname="John"))
+
+# end is a keyword argument
+# for x in range(1, 11):
+#     print(x, end=" ")
+
+# print("1", "2", "3", "4", "5", sep="-")
+
+# Exercise
+
+# def get_phone(country_code, area_code, first, last):
+#     return f"{country_code}-{area_code}-{first}-{last}"
+
+
+# phone_num = get_phone(country_code=92, area_code=123, first=456, last=7890)
+# print(phone_num)
