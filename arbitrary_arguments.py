35a5183e6fdd8ee0c50393fe5dc70975ccb05f4b
Haris Habib <harishabib2@gmail.com>
2025-08-13 16:24:53 +0500

    *args in pythong

---
+++ arbitrary_arguments.py
@@ -0,0 +1,26 @@
+# *args    = allows you to pass multiple non-keyword-arguments
+# **kwargs = allows you to pass multiple keyword-arguments
+#            * unpacking operator
+#            1. Positional, 2. Default, 3. Keyword, 4. Arbitrary
+
+
+# def add(a, b):
+#     return a+b
+
+
+# print(add(1, 3))
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+
+# print(add(1, 3, 4, 5, 3, 1, 2))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end=" ")
+
+
+# display_name("Mr.", "spongebob", "Squarepants", "III")
