e9c0826e13b654ab99788084d042899be859f602
Haris Habib <harishabib2@gmail.com>
2025-08-13 16:37:52 +0500

    new code exercise added

--- arbitrary_arguments_II.py
+++ arbitrary_arguments_II.py
@@ -4,9 +4,38 @@
 #            1. Positional, 2. Default, 3. Keyword, 4. Arbitrary
 
 
-def print_address(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+# def print_address(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
 
 
-print_address(street="123", city="Lahore", state="Punjab", zip="54000")
+# print_address(street="123", city="Lahore", state="Punjab", zip="54000")
+
+
+# def shipping_label(*args, **kwargs):
+#     for arg in args:
+#         print(arg, end=" ")
+#     print()
+#     for value in kwargs.values():
+#         print(value, end=" ")
+
+
+# shipping_label("Dr.", "Noshaad", "Malik", "Chisti", street="123",
+#                city="Lahore", state="Punjab", zip="54000")
+
+
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+    print()
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('pobox')}")
+    else:
+        print(f"{kwargs.get('street')}")
+    print(f"{kwargs.get('city')} {kwargs.get('state')}, {kwargs.get('zip')}")
+
+
+shipping_label("Dr.", "Noshaad", "Malik", "Chisti", street="123",
+               city="Lahore", state="Punjab", pobox="pobox #100", zip="54000")
