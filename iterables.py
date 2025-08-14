7cd3952e53ad119f973d0aa721b49e5c5021ae5f
Haris Habib <harishabib2@gmail.com>
2025-08-14 20:59:18 +0500

    basic uterable

--- iterables.py
+++ iterables.py
@@ -1,1 +1,6 @@
 # Iterables = An Object/Collection that can return elements one at a time allowing to be iterated over in a loop
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
