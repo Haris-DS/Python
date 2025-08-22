908e126ee2d174eda782408229f3275b25f4eade
Haris Habib <harishabib2@gmail.com>
2025-08-22 14:52:27 +0500

    Modules in Python

---
+++ modules.py
@@ -0,0 +1,10 @@
+# # module = a file containing Python definitions and statements
+# # used with modular programming, which is to separate a program into partss
+# # which can be reused in other Python programs
+# # save the file as modules.py to import and use in other files
+
+# print(help("modules"))  # to know more about modules in Python
+
+# import math  # import math module
+# import random  # import random module
+# import datetime  # import datetime module
